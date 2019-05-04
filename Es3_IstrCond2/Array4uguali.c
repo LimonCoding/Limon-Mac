d05c12259555591c4ac036ad77a7ca44010c214d
LimonCoding <50263476+LimonCoding@users.noreply.github.com>
2019-05-04 18:07:17 +0200

    Update Array4uguali.c

--- Es3_IstrCond2/Array4uguali.c
+++ Es3_IstrCond2/Array4uguali.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-int quantiUguali(int *interi, int i);
-int coppia2uguali(int *interi, int i);
+int quantiUguali(int *interi);
+int coppia2uguali(int *interi);
 
 int main() {
 	int interi[4];
@@ -10,19 +10,19 @@ int main() {
 	for(i=0; i<4; i++){
 		scanf("%d", &interi[i]);
 	}
-	uguali=quantiUguali(interi, i);
-	coppia2=coppia2uguali(interi, i);
+	uguali=quantiUguali(interi);
+	if(uguali!=0)
+		coppia2=coppia2uguali(interi);
 
 /* è possibile utilizzare l'istruzione condizionale 'if' con le diverse condizioni */
-
-	if(uguali==1&&coppia2==1)
-		printf("\nCi sono esattamente due coppie di numeri fra loro uguali\n");
-	switch(uguali) { 
+	switch(uguali) {
 		case 0:
 		printf("\nCi sono esattamente 0 numeri fra loro uguali\n");
 		break;
 		case 1:
-		printf("\nCi sono esattamente 2 numeri fra loro uguali\n");
+		/* ATTTENZIONE nello 'switch case' non funziona l'istruzione 'if' */
+		if(coppia2==0)
+			printf("\nCi sono esattamente 2 numeri fra loro uguali\n");
 		break;
 		case 2:
 		printf("\nCi sono esattamente 3 numeri fra loro uguali\n");
@@ -30,26 +30,27 @@ int main() {
 		case 3:
 		printf("\nCi sono esattamente 4 numeri fra loro uguali\n");
 		break;
-	}
+		}
 }
-
-int quantiUguali(int *interi, int i){
-	int same, j;
+/* ATTTENZIONE questa funzione non funziona con gli ultimi numeri quantiUguali
+è necessario che ci sia almeno un numero uguale al primo per ritornare same!=0 */
+int quantiUguali(int *interi){
+	int same, i;
+	printf("sono entrato nella funzione quantiUguali\n" );
 	for(i=0; i<3; i++) {
-		if(interi[i]==interi[i+j])
+		if(interi[0]==interi[i+1])
 			same=same+1;
-			j++;
 	}
 	return same;
 }
 
-int coppia2uguali(int *interi, int i){
-	int same2, k;
-		for(i=0; i<3; i++) {
-			printf("\n%d", interi[i]); 
-		if(interi[i]==interi[i+k])
-			same2=same2+1;
-			k++;
+int coppia2uguali(int *interi) {
+	int same2, i;
+	printf("sono entrato nella funzione coppia2uguali\n" );
+	for(i=0; i<3; i++) {
+		if(interi[i]==interi[i+1]&&interi[i]!=interi[0])
+			printf("\nCi sono esattamente due coppie di numeri fra loro uguali\n");
 	}
+	same2=0;
 	return same2;
 }
