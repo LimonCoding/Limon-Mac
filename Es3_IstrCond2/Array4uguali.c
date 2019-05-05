f6e0ac79652ef50d721e74186fb0610ee1dcf148
LimonCoding <50263476+LimonCoding@users.noreply.github.com>
2019-05-05 21:17:08 +0200

    Array4uguali_CORRETTO.c
    è stato semplificato utilizzando una sola funzione che tramite due cicli for controlla tutti gli elementi e tramite una seconda funzione void stampa il risultato

--- Es3_IstrCond2/Array4uguali.c
+++ Es3_IstrCond2/Array4uguali.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
-int quantiUguali(int *interi);
-int coppia2uguali(int *interi);
+int quantiUguali(int *interi); // confronta i numeri uguali
+int coppia2uguali(int *interi); // confronta eventualmente la seconda coppia uguale
+void stampaCasi(int uguali, int coppia2); // stampa i casi
 
 int main() {
 	int interi[4];
@@ -10,47 +11,46 @@ int main() {
 	for(i=0; i<4; i++){
 		scanf("%d", &interi[i]);
 	}
-	uguali=quantiUguali(interi);
-	if(uguali!=0)
-		coppia2=coppia2uguali(interi);
-
-/* è possibile utilizzare l'istruzione condizionale 'if' con le diverse condizioni */
-	switch(uguali) {
-		case 0:
-		printf("\nCi sono esattamente 0 numeri fra loro uguali\n");
-		break;
-		case 1:
-		/* ATTTENZIONE nello 'switch case' non funziona l'istruzione 'if' */
-		if(coppia2==0)
-			printf("\nCi sono esattamente 2 numeri fra loro uguali\n");
-		break;
-		case 2:
-		printf("\nCi sono esattamente 3 numeri fra loro uguali\n");
-		break;
-		case 3:
-		printf("\nCi sono esattamente 4 numeri fra loro uguali\n");
-		break;
-		}
-}
-/* ATTTENZIONE questa funzione non funziona con gli ultimi numeri quantiUguali
-è necessario che ci sia almeno un numero uguale al primo per ritornare same!=0 */
-int quantiUguali(int *interi){
-	int same, i;
-	printf("sono entrato nella funzione quantiUguali\n" );
-	for(i=0; i<3; i++) {
-		if(interi[0]==interi[i+1])
-			same=same+1;
+	printf("\narray letto:\n");
+	for(i=0; i<4; i++){
+		printf("%d\n", interi[i]);
 	}
+	printf("\n");
+	uguali=quantiUguali(interi);
+	stampaCasi(uguali, coppia2);
+}
+
+int quantiUguali(int *interi){
+	int same, i, j;
+	printf("sono entrato nella funzione quantiUguali\n\n" );
+	for(i=0; i<3; i++){
+		for(j=i+1; j<4; j++){
+			printf("elemento: %d\t=\telemento: %d\n", interi[i], interi[j]);
+			if(interi[i]==interi[j])
+				same=same+1;
+		}
+	}
+	printf("\nuguali: %d\n\n", same);
 	return same;
 }
 
-int coppia2uguali(int *interi) {
-	int same2, i;
-	printf("sono entrato nella funzione coppia2uguali\n" );
-	for(i=0; i<3; i++) {
-		if(interi[i]==interi[i+1]&&interi[i]!=interi[0])
+void stampaCasi(int uguali, int coppia2){
+	printf("sono entrato nella funzione stampaCasi\n");
+		switch(uguali) {
+			case 0:
+			printf("\nCi sono esattamente 0 numeri fra loro uguali\n");
+			break;
+			case 1:
+			printf("\nCi sono esattamente 2 numeri fra loro uguali\n");
+			break;
+			case 2:
 			printf("\nCi sono esattamente due coppie di numeri fra loro uguali\n");
-	}
-	same2=0;
-	return same2;
+			break;
+			case 3:
+			printf("\nCi sono esattamente 3 numeri fra loro uguali\n");
+			break;
+			case 6:
+			printf("\nCi sono esattamente 4 numeri fra loro uguali\n");
+			break;
+			}
 }
